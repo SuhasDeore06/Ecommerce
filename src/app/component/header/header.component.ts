2306247603128f32fe522043284b0a24e33b9c17
SuhasDeore06 <suhas.deore08@gmail.com>
2024-06-14 15:52:40 +0530

    Completed task

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -9,6 +9,7 @@ import { CartService } from 'src/app/service/cart.service';
 export class HeaderComponent implements OnInit {
 
   public totalItem :number = 0;
+  public searchTerm : string = '';
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
@@ -16,5 +17,12 @@ export class HeaderComponent implements OnInit {
       this.totalItem = res.length;
       console.log(this.totalItem,"totalItem");
     })
+  }
+
+  search(event:any)
+  {
+    this.searchTerm = (event.target as HTMLInputElement).value;
+    console.log(this.searchTerm);
+    this.cartService.search.next(this.searchTerm);
   }
 }
--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -10,16 +10,29 @@ import { CartService } from 'src/app/service/cart.service';
 export class ProductsComponent implements OnInit {
 
   public productList:any;
+  public filterCategory :any;
+  list: any;
+  searchKey: string = '';
   constructor(private api: ApiService, private cartServiceL: CartService) { }
 
   ngOnInit(): void {
     this.api.getProduct().subscribe(res => {
       this.productList = res;
       console.log(this.productList);
-
+      this.filterCategory = res;
       this.productList.forEach((a:any)=>{
+        if(a.category === "women's clothing" || a.category === "men's clothing")
+          {
+            a.category = 'fashion';
+          }
          Object.assign(a,{quantity:1, total:a.price});
+         this.list = a.category;
+         
       });
+    })
+
+    this.cartServiceL.search.subscribe((val:any)=>{
+      this.searchKey = val;
     })
   }
 
@@ -28,6 +41,14 @@ export class ProductsComponent implements OnInit {
     this.cartServiceL.addtoCart(item);
   }
 
-
+  filter(category:string)
+  {
+    this.filterCategory = this.productList.filter((a:any)=>{
+      if(a.category == category || category == '')
+        {
+          return a;
+        }
+    })
+  }
 
 }
